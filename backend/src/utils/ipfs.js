1e0878e0826f8f84c3dafcd56082510a83022373
dinesh2104 <dineshsdk56@gmail.com>
2025-04-24 23:19:37 +0530

    upload done

--- backend/src/utils/ipfs.js
+++ backend/src/utils/ipfs.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const FormData = require('form-data');
 const fs = require('fs');
-const path = require('path');
+require('dotenv').config();
 
 class PinataIPFS {
     constructor() {
